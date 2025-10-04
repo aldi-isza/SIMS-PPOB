eacf2fa170e6060408dec0b62f46831916828d35
Aldi Isza <aldyisza87@gmail.com>
2025-10-05 00:07:17 +0700

    feat: Add HomeProvider, SaldoCard, and BannerWidget components; refactor widget exports

---
+++ lib/features/home/data/provider/home_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+
+class HomeProvider extends ChangeNotifier {
+  int _saldo = 0;
+  bool _isHidden = false;
+
+  int get saldo => _saldo;
+  bool get isHidden => _isHidden;
+
+  void setSaldo(int newSaldo) {
+    _saldo = newSaldo;
+    notifyListeners();
+  }
+
+  void toggleVisibility() {
+    _isHidden = !_isHidden;
+    notifyListeners();
+  }
+}
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -2,9 +2,9 @@ export 'package:sims_ppob/core/extensions/int_ext.dart';
 
 export 'buttons/custom_button.dart';
 export 'grid/custom_grid_amount.dart';
-export 'cards/saldo_card.dart';
+export '../../features/home/widgets/saldo_card_widget.dart';
 export 'navigation/custom_nav_item.dart';
 export 'cards/custom_history_card.dart';
 export 'cards/custom_payment_item.dart';
-export 'banners/custom_banner_widget.dart';
+export '../../features/home/widgets/custom_banner_widget.dart';
 export 'profile/profile_picture.dart';
