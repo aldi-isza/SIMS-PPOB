47d1541d9a1a1f2e264ee10780788b88012496f7
Aldi Isza <aldyisza87@gmail.com>
2025-10-08 19:00:38 +0700

    feat: Implement LocalStorageService for token management; update AuthProvide to utilize local storage and add validation methods

--- lib/features/auth/provider/auth_provide.dart
+++ lib/features/auth/provider/auth_provide.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:sims_ppob/core/services/local_storage_service.dart';
 import 'package:sims_ppob/features/auth/data/auth_api.dart';
 import 'package:sims_ppob/features/auth/data/models/login_request.dart';
 import 'package:sims_ppob/features/auth/data/models/login_response.dart';
@@ -6,19 +9,58 @@ import 'package:sims_ppob/features/auth/data/models/register_response.dart';
 
 class AuthProvide extends ChangeNotifier {
   final AuthApi _api = AuthApi();
+  final LocalStorageService _storage = LocalStorageService();
 
   bool _isLoading = false;
+  bool _isAunthenticated = false;
   String? _errorMessage;
+  String? _token;
+
   LoginResponse? _loginResponse;
   RegisterResponse? _registerResponse;
 
+  // GETTER
   bool get isLoading => _isLoading;
+  bool get isAunthenticated => _isAunthenticated;
   String? get errorMessage => _errorMessage;
+  String? get token => _token;
   LoginResponse? get loginResponse => _loginResponse;
   RegisterResponse? get registerResponse => _registerResponse;
 
+  // HELPER
+  void _setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
 
-  
+  void _setError(String? message) {
+    _errorMessage = message;
+    notifyListeners();
+  }
+
+  void _clearError() {
+    _errorMessage = null;
+  }
+
+  // VALIDATION
+  bool _validationEmail(String email) {
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._].\[a-zA-Z0-9]{2,}$',
+    );
+    return emailRegex.hasMatch(email);
+  }
+
+  bool _validatePassword(String password) => password.length >= 8;
+
+  // REGISTER
+  Future<void> Register({
+    required String email,
+    required String firstName,
+    required String lastName,
+    required String password,
+  }) async {
+    if (!_validationEmail(email)) {}
+  }
 
   Future<void> Login({required String email, required String password}) async {
     final emailregExp = RegExp(
